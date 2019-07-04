f286905d13d7ee0434e5a2ab8f3cfcdfc15f3b6f
kv-2 <str_token@mail.ru>
2019-07-04 19:13:57 +0300

    change the migration and db:migrate

--- db/migrate/20190704155715_add_position_to_task.rb
+++ db/migrate/20190704155715_add_position_to_task.rb
@@ -1,5 +1,8 @@
 class AddPositionToTask < ActiveRecord::Migration[5.2]
   def change
     add_column :tasks, :position, :integer
+    Task.order(:updated_at).each_index(1) do |task, index|
+      task.update_column :position, index
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_04_152754) do
+ActiveRecord::Schema.define(version: 2019_07_04_155715) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
   end
 
 end
