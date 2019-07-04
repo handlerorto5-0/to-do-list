7f46d6207b559e9c698a4e3d1eb4da45a13c0701
kv-2 <str_token@mail.ru>
2019-07-04 19:26:23 +0300

    update the rask's models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,17 @@
 class Task < ApplicationRecord
+  # gem add new methids:
+  # task.move_lower
+  # task.move_higher
+  # task.move_to_bttom
+  # task.move_to_top
+  # task.first?
+  # task.last?
+  # task.in_list?
+  # task.not_in_list?
+  # task.higher_item?
+  # task.higher_items?
+  # task.lower_item?
+  # task.lower_items?
+  acts_as_list  
+  
 end
